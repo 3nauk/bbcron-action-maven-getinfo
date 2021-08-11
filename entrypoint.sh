8ffdc51aa0f48d903259434c9bc3269cec6f82d0
benauca <benauca@gmail.com>
2021-08-11 08:29:41 +0200

    Include test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 
 #!/bin/sh -l 
 POM_PATH="${1-.}" 
+echo ${POM_PATH}
+ 
 POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 POM_ARTIFACT=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
 POM_GROUP=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.groupId -q -DforceStdout)
