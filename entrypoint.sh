8cab5e0f4626aab8c992e748d28022796d18edc8
benauca <benauca@gmail.com>
2021-08-11 08:53:50 +0200

    Include bin bash entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
-
 #!/bin/sh -l 
 
+#POM_PATH="${1-.}" 
+POM_PATH="./test/resources" 
+POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
+POM_ARTIFACT=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+POM_GROUP=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.groupId -q -DforceStdout)
 echo "Founded version " 
-
+echo "Founded version ${POM_VERSION} artifactId ${POM_ARTIFACT} groupId ${POM_GROUP} "
+echo ::set-output name=version::$POM_VERSION
+echo ::set-output name=artifactId::$POM_ARTIFACT
+echo ::set-output name=groupId::$POM_GROUP
