8b854ab96440e65cd1afd47356207136d252c382
Benauca <benauca@gmail.com>
2021-09-03 12:45:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 POM_PATH=${1-.}
 echo "------------ ${GITHUB_WORKSPACE} "
-ls -lta
+ls -lta ${GITHUB_WORKSPACE}
 POM_PATH=${GITHUB_WORKSPACE}
 POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
 POM_ARTIFACT=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
