db804550fdf67e111235aa72bcf310007f38e5ae
Benauca <benauca@gmail.com>
2021-09-06 11:04:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh -l 
 env
-ls -la
 POM_PATH=${1-.}
-POM_VERSION=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
-POM_ARTIFACT=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
-POM_GROUP=$(mvn -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.groupId -q -DforceStdout)
+POM_VERSION=$(mvn -s ./settings -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.version -q -DforceStdout)
+POM_ARTIFACT=$(mvn -s ./settings -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.artifactId -q -DforceStdout)
+POM_GROUP=$(mvn -s ./settings -f $POM_PATH/pom.xml help:evaluate -Dexpression=project.groupId -q -DforceStdout)
 echo "Founded version " 
 echo "Founded version ${POM_VERSION} artifactId ${POM_ARTIFACT} groupId ${POM_GROUP} "
 echo ::set-output name=version::$POM_VERSION
